c52dcdf109887b6fe5117689014b999b8ce9e1fc
AresOsredkar <aresosredkar1337@gmail.com>
2022-08-22 19:26:11 +0200

    Added mobile layout

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,24 +2,36 @@ import '../Button/Button.scss'
 import {NavLink} from 'react-router-dom'
 import './Navigation.scss'
 import {useState} from 'react'
-import { FaStream } from 'react-icons/fa'
+import {  FaStream } from 'react-icons/fa'
 import meal from '../../Assets/meal.png'
 
-const Items = ["Home", "Categories", "Contact", "Settings"];
+const Items = ["Home", "Categories", "Contact"];
 
 const NavigationBar = () => {
-    const [isMenuExtended,setIsMenuExtended] = useState(false)
+    const [isMobileMenuExtended,setIsMobileMenuExtended] = useState(false)
+    const [isSettingsMenuExtended,setIsSettingsMenuExtended] = useState(false)
 
-    const handleClick = () => setIsMenuExtended(prevState=>!prevState)
+    const handleDropdownClick = () => setIsMobileMenuExtended((prevState)=>!prevState)
 
-    return (<div id="navigation">
-                <img src={meal} height="80px" alt='MealsLogo'/>
-                <div id="navBar">
-                    {Items.map(item => <NavLink to={`/${item}`} className='button button-primary'>{item}</NavLink>)}
+    const handleSettingsClick = () => setIsSettingsMenuExtended((prevState)=>false)
+
+    return (<div id="headerContainer">
+                <div id="navigation">
+                    <NavLink to='/Home' style={{border:'none'}}><img src={meal} height="80px" alt='MealsLogo' className='logo'/></NavLink>
+                    <div id="navBar">
+                        {Items.map(item => <NavLink key={item} to={`/${item}`} className='button button-primary'>{item}</NavLink>)}
+                        <div className='button button-primary' onClick={handleSettingsClick}>
+                            Settings
+                        </div>
+                        
+                    </div>
+                    <FaStream className='menuButton' onClick={handleDropdownClick}/>
                 </div>
-                <FaStream className='menuButton' onClick={handleClick}/>
-                <div id="navBox">
-
+                <div className={`navBox${isMobileMenuExtended ? 'Shown' : 'Hidden'}`}>
+                    {Items.map(item => <NavLink key={item} onClick={handleDropdownClick} to={`/${item}`} className='button button-primary button-hamburger'>{item}</NavLink>)}
+                </div>
+                <div id="settingsBox" style={{display: `${isSettingsMenuExtended ? 'flex' : 'none'}`}}>  
+                    Placeholder
                 </div>
             </div>
             );
